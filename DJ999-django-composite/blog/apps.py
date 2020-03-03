5ef5291d29554adff481d8a713cd54cc3bb206bf
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-03-03 11:20:39 +0800

    Updated DJ999-django-composite

--- DJ999-django-composite/blog/apps.py
+++ DJ999-django-composite/blog/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
+    """
+    https://docs.djangoproject.com/en/3.0/ref/applications/#django.apps.AppConfig
+    """
     name = 'blog'
     verbose_name = '博客'
