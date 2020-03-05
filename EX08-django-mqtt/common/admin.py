c005e1f21e0135542c1f1585d81b89fc4844c512
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-03-05 14:08:26 +0800

    Updated EX08-django-mqtt

--- EX08-django-mqtt/common/admin.py
+++ EX08-django-mqtt/common/admin.py
@@ -5,10 +5,23 @@ from django.contrib import admin
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django_mqtt.publisher.models import Data as MQTTData
+from django_mqtt.publisher.models import Client
+from django_mqtt.publisher.signals import mqtt_publish
+
+
+def then_publish(sender, client, userdata, mid, **kwargs):
+    if not isinstance(client, Client):
+        raise AttributeError('client must be Client object')
+    print(mid)
+
+
+mqtt_publish.connect(receiver=then_publish, sender=Client,
+                     dispatch_uid='my_django_mqtt_then_publish')
 
 
 @receiver(post_save, sender=MQTTData)
 def auto_update(sender, instance, **kwargs):
     """添加或更新 Data 的时候会发布消息"""
+    print(kwargs)
     print(f'publish to {instance.topic.name}')
     instance.update_remote()
