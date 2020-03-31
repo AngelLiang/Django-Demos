a38bcbd890e7fdf0a3f6275f46f49fac48bab804
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-03-31 14:42:45 +0800

    Updated EX06-django-restful-api

--- EX06-django-restful-api/snippets/urls.py
+++ EX06-django-restful-api/snippets/urls.py
@@ -1,3 +1,4 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
@@ -41,10 +42,16 @@ urlpatterns = [
 
 # https://www.django-rest-framework.org/tutorial/6-viewsets-and-routers/#using-routers
 
-from rest_framework.routers import DefaultRouter
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'snippets', views6.SnippetViewSet)
+
+# basename - the base to use for the URL names that are created.
+# action - the name of the current action (e.g., list, create).
+# detail - boolean indicating if the current action is configured for a list or detail view.
+# suffix - the display suffix for the viewset type - mirrors the detail attribute.
+# name - the display name for the viewset. This argument is mutually exclusive to suffix.
+# description - the display description for the individual view of a viewset.
+router.register(r'snippets', views6.SnippetViewSet, basename='snippets')
 # router.register(r'users', views.UserViewSet)
 
 # The API URLs are now determined automatically by the router.
