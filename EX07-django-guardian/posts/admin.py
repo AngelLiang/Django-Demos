9abb4ea71ead40c67814f82fc03ac238c8466860
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-04-21 14:35:38 +0800

    Updated EX07-django-guardian

--- EX07-django-guardian/posts/admin.py
+++ EX07-django-guardian/posts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Task
 
 
 # class PostAdmin(admin.ModelAdmin):
@@ -18,6 +18,10 @@ from .models import Post
 from guardian.admin import GuardedModelAdmin
 
 
+class TaskAdmin(GuardedModelAdmin):
+    pass
+
+
 class PostAdmin(GuardedModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ('title', 'slug', 'created_at')
@@ -27,3 +31,4 @@ class PostAdmin(GuardedModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Task, TaskAdmin)
