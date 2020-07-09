dd604167e2b6d339f9bf23f30c297b739e719dbd
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-07-09 18:07:53 +0800

    Updated MY01-simple-statistics， 添加搜索字段

--- MY01-simple-statistics/order/admin.py
+++ MY01-simple-statistics/order/admin.py
@@ -105,10 +105,11 @@ class OrderItemInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'order_date', 'title', 'amount', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('title',)
+
     readonly_fields = ('amount',)
     inlines = (OrderItemInline,)
-    list_filter = ('created_at',)
-
     ordering = ('created_at',)
     date_hierarchy = 'created_at'
 
