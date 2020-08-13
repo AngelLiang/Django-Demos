3561fff0de0c0c0d598c3a5772106c5c9d11027a
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-08-13 14:40:24 +0800

    feat(MY05-rworkflow): 添加创建者和更新者模型字段，并自动记录

--- MY05-rworkflow/rworkflow/admin/base.py
+++ MY05-rworkflow/rworkflow/admin/base.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 
 class BaseAdmin(admin.ModelAdmin):
-    pass
+    view_on_site = False
+
+    def save_model(self, request, obj, form, change):
+        user = request.user
+        obj.updated_by = user.username
+        if not change:
+            obj.created_by = user.username
+        super().save_model(request, obj, form, change)
--- MY05-rworkflow/rworkflow/models/base.py
+++ MY05-rworkflow/rworkflow/models/base.py
@@ -11,3 +11,9 @@ class BaseModel(models.Model):
 
     created_at = models.DateTimeField(_('创建时间'), null=True, blank=True, auto_now_add=True)
     updated_at = models.DateTimeField(_('更新时间'), null=True, blank=True, auto_now=True)
+
+    #################################################################
+    # 使用用户名作为创建者和修改者的值
+    #################################################################
+    created_by = models.CharField(_('创建者'), max_length=80, default='', blank=True, db_index=True)
+    updated_by = models.CharField(_('修改者'), max_length=80, default='', blank=True)
