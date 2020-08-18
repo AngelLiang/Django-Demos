db4446565f8d6574e68634f2ca0733f3f490072c
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-08-18 15:49:57 +0800

    to(MY05-rworkflow): 更新批准规则表单字段的help_text

--- MY05-rworkflow/rworkflow/forms/transitionapprovalmeta.py
+++ MY05-rworkflow/rworkflow/forms/transitionapprovalmeta.py
@@ -2,9 +2,20 @@ from django import forms
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext_lazy as _
+from django.utils.html import format_html, format_html_join
 
 from ..models import TransitionApprovalMeta, TransitionMeta
 from ..core.rule_parser import RuleParser, RuleEvaluationError
+
+
+def _rule_help_text_html():
+    help_texts = [
+        '格式为 ["操作符", ["操作符1", "参数1", "参数2", ...], ["操作符2", "参数3", "参数4", ...]]',
+        '可以使用 "额外参数编号" 格式来代表额外参数的数值，例如："EX001"',
+        '操作符包括： =, !=, >, >=, <, <=, and, or, not, in, str, int, +, -, *, / ',
+    ]
+    help_items = format_html_join('', '<li>{}</li>', ((help_text,) for help_text in help_texts))
+    return format_html('<ul>{}</ul>', help_items) if help_items else ''
 
 
 class TransitionApprovalMetaForm(forms.ModelForm):
@@ -26,7 +37,7 @@ class TransitionApprovalMetaForm(forms.ModelForm):
         model = TransitionApprovalMeta
         fields = '__all__'
         help_texts = {
-            'rule': _('格式为 ["操作符", ["操作符1", "参数1", "参数2", ...], ["操作符2", "参数1", "参数2", ...]]')
+            'rule': _rule_help_text_html
         }
 
     def __init__(self, *args, **kwargs):
