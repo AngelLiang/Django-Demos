9724bcf08941627de450fdca1de2175d089a3ac1
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2021-08-19 11:44:38 +0800

    Add DJ43-django-avatar

---
+++ DJ43-django-avatar/order/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+
+from . import models
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'avatar')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class OrderItemInline(admin.TabularInline):
+    model = models.OrderItem
+    extra = 1
+    fields = ('product', 'quantity', 'price', 'amount',)
+    readonly_fields = ('amount',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'order_date', 'title', 'amount')
+    readonly_fields = ('amount',)
+    inlines = (OrderItemInline,)
+
+    # def save_model(self, request, obj, form, change):
+    #     super().save_model(request, obj, form, change)
+    #     obj.update_amount()
+
+
+admin.site.register(models.Customer, CustomerAdmin)
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Order, OrderAdmin)
